7b35f6a0bd91ca2cfc2a1ce6aa3e206ed4cadf44
Yoga Wahyu <yogawahyuy@gmail.com>
2023-01-05 15:10:32 +0700

    update

--- app/src/main/java/com/systudio/datacovid19/view/BarChartActivity.kt
+++ app/src/main/java/com/systudio/datacovid19/view/BarChartActivity.kt
@@ -1,21 +1,19 @@
 package com.systudio.datacovid19.view
 
 import android.graphics.Color
-import android.opengl.Visibility
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Paint
 import android.os.Bundle
 import android.util.Log
-import android.view.ContextMenu
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.github.mikephil.charting.animation.Easing
-import com.github.mikephil.charting.charts.BarChart
-import com.github.mikephil.charting.charts.CombinedChart
 import com.github.mikephil.charting.components.Legend
 import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.YAxis
 import com.github.mikephil.charting.data.*
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 import com.github.mikephil.charting.utils.MPPointF
@@ -24,6 +22,7 @@ import com.systudio.datacovid19.model.ListData
 import com.systudio.datacovid19.utils.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_bar_chart.*
+
 
 @AndroidEntryPoint
 class BarChartActivity : AppCompatActivity() {
@@ -45,6 +44,8 @@ class BarChartActivity : AppCompatActivity() {
             lineChart.visibility = View.GONE
             piechart.visibility = View.GONE
             stackbarchart.visibility = View.GONE
+            combinedChart.visibility = View.GONE
+            candleStickChart.visibility = View.GONE
         }
         if (item.itemId==R.id.groupchartmenu){
             barchart.visibility = View.GONE
@@ -52,6 +53,8 @@ class BarChartActivity : AppCompatActivity() {
             lineChart.visibility = View.GONE
             piechart.visibility = View.GONE
             stackbarchart.visibility = View.GONE
+            combinedChart.visibility = View.GONE
+            candleStickChart.visibility = View.GONE
         }
         if (item.itemId==R.id.linechartmenu){
             barchart.visibility = View.GONE
@@ -59,6 +62,8 @@ class BarChartActivity : AppCompatActivity() {
             lineChart.visibility = View.VISIBLE
             piechart.visibility = View.GONE
             stackbarchart.visibility = View.GONE
+            combinedChart.visibility = View.GONE
+            candleStickChart.visibility = View.GONE
         }
         if (item.itemId==R.id.piechartmenu){
             barchart.visibility = View.GONE
@@ -66,6 +71,8 @@ class BarChartActivity : AppCompatActivity() {
             lineChart.visibility = View.GONE
             piechart.visibility = View.VISIBLE
             stackbarchart.visibility = View.GONE
+            combinedChart.visibility = View.GONE
+            candleStickChart.visibility = View.GONE
         }
         if (item.itemId == R.id.stackbarchartmenu){
             barchart.visibility = View.GONE
@@ -73,6 +80,26 @@ class BarChartActivity : AppCompatActivity() {
             lineChart.visibility = View.GONE
             piechart.visibility = View.GONE
             stackbarchart.visibility = View.VISIBLE
+            combinedChart.visibility = View.GONE
+            candleStickChart.visibility = View.GONE
+        }
+        if (item.itemId == R.id.combinedChartmenu){
+            barchart.visibility = View.GONE
+            groupbarchart.visibility = View.GONE
+            lineChart.visibility = View.GONE
+            piechart.visibility = View.GONE
+            stackbarchart.visibility = View.GONE
+            combinedChart.visibility = View.VISIBLE
+            candleStickChart.visibility = View.GONE
+        }
+        if (item.itemId == R.id.candlestickmenu){
+            barchart.visibility = View.GONE
+            groupbarchart.visibility = View.GONE
+            lineChart.visibility = View.GONE
+            piechart.visibility = View.GONE
+            stackbarchart.visibility = View.GONE
+            combinedChart.visibility = View.GONE
+            candleStickChart.visibility = View.VISIBLE
         }
         return super.onOptionsItemSelected(item)
     }
@@ -85,6 +112,8 @@ class BarChartActivity : AppCompatActivity() {
                 setupLineChart(it)
                 setupPieChart(it)
                 setupStackedBarChart(it)
+                setupCombinedChart(it)
+                setupCandleStickChart(it)
             }
         })
         viewModel.fetchAllData()
@@ -115,6 +144,7 @@ class BarChartActivity : AppCompatActivity() {
     private fun setupGroupChart(listData: List<ListData>){
         groupbarchart.setTouchEnabled(true)
         groupbarchart.setPinchZoom(true)
+        groupbarchart.isDragEnabled = true
         groupbarchart.description.isEnabled = false
         groupbarchart.xAxis?.position = XAxis.XAxisPosition.BOTTOM
         groupbarchart.xAxis?.granularity = 1f
@@ -134,16 +164,16 @@ class BarChartActivity : AppCompatActivity() {
         legend.orientation = Legend.LegendOrientation.HORIZONTAL
         legend.setDrawInside(true)
 
-//        val kasus = ArrayList<BarEntry>()
-//        for (i in 0..5 ){
-//            kasus.add(BarEntry(i.toFloat(),listData.get(i).jumlah_kasis.toFloat()))
-//        }
+        val kasus = ArrayList<BarEntry>()
+        for (i in 0..5){
+            kasus.add(BarEntry(i.toFloat(),listData.get(i).jumlah_kasis.toFloat()))
+        }
         val sembuh = ArrayList<BarEntry>()
-        for (i in 0..5 ){
+        for (i in 0..5){
             sembuh.add(BarEntry(i.toFloat(),listData.get(i).jumlah_sembuh.toFloat()))
         }
         val meninggal = ArrayList<BarEntry>()
-        for (i in 0..5 ){
+        for (i in 0..5){
             meninggal.add(BarEntry(i.toFloat(),listData.get(i).jumlah_meninggal.toFloat()))
         }
         val dirawat = ArrayList<BarEntry>()
@@ -151,8 +181,8 @@ class BarChartActivity : AppCompatActivity() {
             dirawat.add(BarEntry(i.toFloat(),listData.get(i).jumlah_dirawat.toFloat()))
         }
 
-//        val kasusBarDataSet = BarDataSet(kasus,"Kasus")
-//        kasusBarDataSet.color = Color.BLUE
+        val kasusBarDataSet = BarDataSet(kasus,"Kasus")
+        kasusBarDataSet.color = Color.BLUE
         val sembuhBarDataSet = BarDataSet(sembuh,"Sembuh")
         sembuhBarDataSet.color = Color.GREEN
         val dirawatBarDataSet = BarDataSet(dirawat,"Dirawat")
@@ -160,7 +190,7 @@ class BarChartActivity : AppCompatActivity() {
         val meninggalBarDataSet = BarDataSet(meninggal,"Meninggal")
         meninggalBarDataSet.color = Color.RED
 
-        val groupBar = BarData(sembuhBarDataSet,dirawatBarDataSet,meninggalBarDataSet)
+        val groupBar = BarData(sembuhBarDataSet,dirawatBarDataSet,meninggalBarDataSet,kasusBarDataSet)
         groupBar.barWidth = 0.25f
         groupbarchart.data = groupBar
         groupbarchart.groupBars(0f,0.07f,0.03f)
@@ -202,6 +232,9 @@ class BarChartActivity : AppCompatActivity() {
 
         stackbarchart.description.isEnabled = false
         stackbarchart.xAxis.position = XAxis.XAxisPosition.BOTTOM
+        stackbarchart.setTouchEnabled(true)
+        stackbarchart.isDragEnabled = true
+        stackbarchart.setScaleEnabled(true)
         stackbarchart.data = BarData(sembuhBarDataSet,dirawatBarDataSet,meninggalBarDataSet)
         stackbarchart.animateXY(200,500)
     }
@@ -273,6 +306,9 @@ class BarChartActivity : AppCompatActivity() {
         piechart.description.isEnabled = false
         piechart.setExtraOffsets(5f,10f,5f,5f)
         piechart.dragDecelerationFrictionCoef=0.95f
+        piechart.centerText = "Jumlah Sembuh"
+        piechart.setCenterTextColor(Color.BLACK)
+        piechart.setDrawCenterText(true)
 
         piechart.isDrawHoleEnabled = true
         piechart.setHoleColor(Color.WHITE)
@@ -283,16 +319,15 @@ class BarChartActivity : AppCompatActivity() {
         piechart.holeRadius =58f
         piechart.transparentCircleRadius = 61f
 
-        piechart.setDrawCenterText(false)
         piechart.rotationAngle = 0f
 
         piechart.isRotationEnabled = true
         piechart.isHighlightPerTapEnabled = true
 
-        piechart.animateY(1400,Easing.EaseInOutQuad)
+        piechart.animateY(1000,Easing.EaseInOutQuad)
         piechart.legend.isEnabled = false
         piechart.setEntryLabelColor(Color.WHITE)
-        piechart.setEntryLabelTextSize(12f)
+        piechart.setEntryLabelTextSize(0f)
 
         piechart.highlightValue(null)
 
@@ -304,7 +339,7 @@ class BarChartActivity : AppCompatActivity() {
         val sembuhPieDataSet = PieDataSet(sembuh,"")
         sembuhPieDataSet.setDrawIcons(false)
         sembuhPieDataSet.sliceSpace = 6f
-        sembuhPieDataSet.iconsOffset = MPPointF(0f,40f)
+        sembuhPieDataSet.iconsOffset = MPPointF(10f,70f)
         sembuhPieDataSet.selectionShift = 5f
 
         val color = ArrayList<Int>()
@@ -319,16 +354,15 @@ class BarChartActivity : AppCompatActivity() {
         val legend = piechart.legend
         piechart.legend.isWordWrapEnabled = true
         piechart.legend.isEnabled = true
-        legend.verticalAlignment = Legend.LegendVerticalAlignment.BOTTOM
-        legend.horizontalAlignment = Legend.LegendHorizontalAlignment.CENTER
+        legend.verticalAlignment = Legend.LegendVerticalAlignment.CENTER
+        legend.horizontalAlignment = Legend.LegendHorizontalAlignment.RIGHT
         legend.formSize = 20f
         legend.formToTextSpace = 0f
         legend.form = Legend.LegendForm.CIRCLE
         legend.textSize = 10f
-        legend.orientation = Legend.LegendOrientation.HORIZONTAL
+        legend.orientation = Legend.LegendOrientation.VERTICAL
         legend.isWordWrapEnabled = true
         legend.setDrawInside(false)
-
 
         val data = PieData(sembuhPieDataSet)
         piechart.data = data
@@ -340,9 +374,151 @@ class BarChartActivity : AppCompatActivity() {
         combinedChart.setDrawGridBackground(false)
         combinedChart.setDrawBarShadow(false)
         combinedChart.isHighlightFullBarEnabled = false
+        combinedChart.setTouchEnabled(true)
+        combinedChart.setPinchZoom(true)
+        combinedChart.isDragXEnabled = true
 
-//        val combineddrawOrder = CombinedChart.DrawOrder{}
-//        combinedChart.drawOrder =
+        val label = ArrayList<String>()
+        for (i in 0..5){
+            label.add(listData.get(i).key)
+        }
+
+        val legend = combinedChart.legend
+        legend.isWordWrapEnabled = true
+        legend.verticalAlignment = Legend.LegendVerticalAlignment.BOTTOM
+        legend.horizontalAlignment = Legend.LegendHorizontalAlignment.CENTER
+        legend.orientation = Legend.LegendOrientation.HORIZONTAL
+        legend.setDrawInside(false)
+
+        val rightAxis = combinedChart.axisRight
+        rightAxis.setDrawGridLines(false)
+        rightAxis.axisMinimum = 0f
+
+        val leftAxis = combinedChart.axisLeft
+        leftAxis.setDrawGridLines(false)
+        leftAxis.axisMinimum = 0f
+
+        val axis = combinedChart.xAxis
+        axis.position = XAxis.XAxisPosition.BOTTOM
+        axis.axisMinimum = 0f
+        axis.granularity = 1f
+        axis.valueFormatter = IndexAxisValueFormatter(label)
+
+        val combinedata = CombinedData()
+
+        combinedata.setData(generateBarData(listData))
+        combinedata.setData(generateLineData(listData))
+
+        combinedChart.data = combinedata
+    }
+
+    private fun generateLineData(listData: List<ListData>) : LineData{
+        val linedata = LineData()
+        val sembuh = ArrayList<Entry>()
+        for (i in 0..5 ){
+            sembuh.add(Entry(i.toFloat(),listData.get(i).jumlah_sembuh.toFloat()))
+        }
+        val meninggal = ArrayList<Entry>()
+        for (i in 0..5 ){
+            meninggal.add(Entry(i.toFloat(),listData.get(i).jumlah_meninggal.toFloat()))
+        }
+        val dirawat = ArrayList<Entry>()
+        for (i in 0..5 ){
+            dirawat.add(Entry(i.toFloat(),listData.get(i).jumlah_dirawat.toFloat()))
+        }
+
+        val sembuhLineDataset = LineDataSet(sembuh,"Sembuh")
+        sembuhLineDataset.mode = LineDataSet.Mode.CUBIC_BEZIER
+        sembuhLineDataset.color = Color.GREEN
+        sembuhLineDataset.circleRadius = 5f
+        sembuhLineDataset.setCircleColor(Color.GREEN)
+
+        val dirawatLineDataSet = LineDataSet(dirawat,"dirawat")
+        dirawatLineDataSet.mode = LineDataSet.Mode.CUBIC_BEZIER
+        dirawatLineDataSet.color = Color.GRAY
+        dirawatLineDataSet.circleRadius = 5f
+        dirawatLineDataSet.setCircleColor(Color.GRAY)
+
+        val meninggalLineDataset = LineDataSet(meninggal,"Meninggal")
+        meninggalLineDataset.mode = LineDataSet.Mode.CUBIC_BEZIER
+        meninggalLineDataset.color = Color.RED
+        meninggalLineDataset.circleRadius = 5f
+        meninggalLineDataset.setCircleColor(Color.RED)
+
+        linedata.addDataSet(meninggalLineDataset)
+        linedata.addDataSet(dirawatLineDataSet)
+        linedata.addDataSet(sembuhLineDataset)
+        return linedata
+    }
+
+    private fun generateBarData(listData: List<ListData>): BarData {
+        val barEntriesList = ArrayList<BarEntry>()
+
+        for (i in 0..5) {
+            barEntriesList.add(BarEntry(i.toFloat(), listData.get(i).jumlah_kasis.toFloat()))
+        }
+        val meninggal = ArrayList<BarEntry>()
+        for (i in 0..5 ){
+            meninggal.add(BarEntry(i.toFloat(),listData.get(i).jumlah_meninggal.toFloat()))
+        }
+        val barDataset = BarDataSet(barEntriesList, "Jumlah Kasus")
+        barDataset.color = Color.BLUE
+        val meninggalDataSet = BarDataSet(meninggal,"")
+        meninggalDataSet.color = Color.RED
+
+        return BarData(barDataset)
+    }
+
+    private fun setupCandleStickChart(listData: List<ListData>){
+        candleStickChart.setMaxVisibleValueCount(60)
+       // candleStickChart.setDrawBorders(true)
+        //candleStickChart.isHighlightPerDragEnabled = true
+        candleStickChart.setBorderColor(Color.LTGRAY)
+        val axisLeft = candleStickChart.axisLeft
+        val rightAxis = candleStickChart.axisRight
+
+        axisLeft.setDrawGridLines(false)
+        axisLeft.setLabelCount(7,false)
+        rightAxis.isEnabled = false
+        candleStickChart.requestDisallowInterceptTouchEvent(true)
+
+        val xAxis = candleStickChart.xAxis
+        xAxis.position = XAxis.XAxisPosition.BOTTOM
+        xAxis.setDrawGridLines(false)
+        xAxis.setDrawLabels(false)
+        rightAxis.textColor = Color.WHITE
+        axisLeft.setDrawLabels(false)
+        xAxis.granularity = 1f
+        xAxis.isGranularityEnabled = true
+        xAxis.setAvoidFirstLastClipping(true)
+
+        val legend = candleStickChart.legend
+        legend.isEnabled = false
+
+        val candleEntry = ArrayList<CandleEntry>()
+        for (i in 0..5 ){
+//            candleEntry.add(CandleEntry(i.toFloat(),listData.get(i).jumlah_kasis.toFloat(),listData.get(i).jumlah_sembuh.toFloat(),
+//            listData.get(i).jumlah_sembuh.toFloat(),listData.get(i).jumlah_dirawat.toFloat()))
+            candleEntry.add(CandleEntry(0f, 225f, 219.84f, 224.94f, 221.07f))
+            candleEntry.add(CandleEntry(1f, 228.35f, 222.57f, 223.52f, 226.41f))
+            candleEntry.add(CandleEntry(2f, 226.84f, 222.52f, 225.75f, 223.84f))
+            candleEntry.add(CandleEntry(3f, 222.95f, 217.27f, 222.15f, 217.88f))
+        }
+
+        val candleDataSet = CandleDataSet(candleEntry,"Dataset1")
+        candleDataSet.axisDependency = YAxis.AxisDependency.LEFT
+        //candleDataSet.color = Color.rgb(80, 80, 80)
+        candleDataSet.shadowColor = Color.LTGRAY
+        candleDataSet.shadowWidth = 0.8f
+        candleDataSet.decreasingColor = Color.RED
+        candleDataSet.decreasingPaintStyle = Paint.Style.FILL
+        candleDataSet.increasingColor = Color.rgb(122, 242, 84)
+        candleDataSet.increasingPaintStyle = Paint.Style.STROKE
+        candleDataSet.neutralColor = Color.BLUE
+
+        val data = CandleData(candleDataSet)
+
+        candleStickChart.data = data
     }
 
 }
